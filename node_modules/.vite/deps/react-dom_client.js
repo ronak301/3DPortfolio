e9a1a23e8d91b398e103f1315a20d385cee5c861
Ronak Kothari <ronakkothari301@gmail.com>
2024-02-08 23:41:54 +0530

    initial setup

---
+++ node_modules/.vite/deps/react-dom_client.js
@@ -0,0 +1,8 @@
+import {
+  require_client
+} from "./chunk-DSI3WPTC.js";
+import "./chunk-ZW7WJ6XU.js";
+import "./chunk-4D5CYJYK.js";
+import "./chunk-CEQRFMJQ.js";
+export default require_client();
+//# sourceMappingURL=react-dom_client.js.map
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Html, useProgress } from "@react-three/drei";
+
+const Loader = () => {
+  const {progress} = useProgress();
+  return <div>Loader</div>;
+};
+
+export default Loader;
