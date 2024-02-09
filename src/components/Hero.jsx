7203fe6cd80f3430dc7cb9bf07303d123fe86499
Ronak Kothari <ronakkothari301@gmail.com>
2024-02-09 14:57:48 +0530

    updating name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Ronak</span>
+            Hi, I'm <span className="text-[#915eff]">Ronak Kothari</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop mobile apps, webapps, user{" "}
